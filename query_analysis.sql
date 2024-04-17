af9cdf866edbd5a283777783b490d5b881d6e171
juanartiandy <106533560+juanartiandy@users.noreply.github.com>
2024-04-17 22:28:20 +0700

    Update query_analysis.sql

--- query_analysis.sql
+++ query_analysis.sql
@@ -1,3 +1,10 @@
+-- This SQL Project is aiming to do these exercises: 
+-- 1. Analyzing Product Sales Trends
+-- 2. Analyzing Customer Purchasing Behavior
+-- 3. Identifying Top 10 Selling Products
+-- 4. Analyzing Regional Sales Trends
+-- 5. Revenue by Promotion Type
+-------------------------------------------------------------------------
 -- Sales trends by product category
 SELECT
     product_category,
